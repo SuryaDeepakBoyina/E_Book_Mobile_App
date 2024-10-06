f1c59a809cf846afbfeacf5c23a22df15a710117
imSuryaD <bsuryadeepak@gmail.com>
2024-10-06 19:08:53 +0530

    Fifth_Commit_Server_Creted_With_nodemon_express_node_modules_git_ignore

--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,8 @@ const express = require("express");
 const app = express();
 
 app.listen(2000,()=>{
-    console.log("Connected to serer at 2000");
+    console.log("Server is running on port 2000");
+});
+app.get("/",(req,res)=>{
+    res.send("Hello World123");
 });
